2f266b68c2474fc9aeb4b010ada1627b3dea7b18
謝志斌 Tommy <tommy@miniasp.com>
2021-10-21 14:24:30 +0800

    type and PropType

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="app">
-    <JobsList :jobs="jobs" />
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+    <JobsList :jobs="jobs" :order="order" />
   </div>
 </template>
 ㄑ
@@ -8,6 +15,7 @@
 import { defineComponent, reactive, ref, toRefs } from "vue";
 import JobsList from "./components/JobsList.vue";
 import Job from "./types/Job";
+import OrderTerm from "./types/OrderTerm";
 
 export default defineComponent({
   name: "App",
@@ -45,10 +53,31 @@ export default defineComponent({
         id: "5",
       },
     ]);
-    return { jobs };
+
+    const order = ref<OrderTerm>("title");
+    const handleClick = (term: OrderTerm) => {
+      order.value = term;
+    };
+    return { jobs, handleClick, order };
   },
 });
 </script>
 
 <style>
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
 </style>
--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="job-list">
+      <p>Ordered by {{order}}</p>
     <ul>
       <li v-for="job in jobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
@@ -21,13 +22,18 @@
 
 <script lang="ts">
 import Job from "@/types/Job";
+import OrderTerm from "@/types/OrderTerm";
 import { defineComponent, PropType } from "vue";
 
 export default defineComponent({
   props: {
     jobs: {
       required: true,
-      type: Array as PropType<Job[]>,
+      type: Array as PropType<Job[]>, // type assertion, Vue uses JavaScript to examine type, so we need to use JavaScript type then use PropType type assertion
+    },
+    order: {
+      required: true,
+      type: String as PropType<OrderTerm>,
     },
   },
 });
